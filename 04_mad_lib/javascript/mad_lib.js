2f34773911bec9cedcc87abe959dbe641b1ba1ca
Chet Bramble <cjbramble@gmail.com>
2020-08-07 18:19:47 -0400

    Mad lib (#5)

---
+++ 04_mad_lib/javascript/mad_lib.js
@@ -0,0 +1,6 @@
+var noun = window.prompt("What is your noun?");
+var verb = window.prompt("What is your verb?");
+var adjective = window.prompt("What is your adjective?");
+var adverb = window.prompt("What is your adverb?");
+
+console.log(`Do you ${verb} your ${adjective} ${noun} ${adverb}? \nThat's hilarious!`);
---
+++ 04_mad_lib/python/mad_lib.py
@@ -0,0 +1,30 @@
+def get_noun():
+    noun = input("Enter a noun: ")
+    return noun
+
+
+def get_verb():
+    verb = input("Enter a verb: ")
+    return verb
+
+
+def get_adjective():
+    adjective = input("Enter an adjective: ")
+    return adjective
+
+
+def get_adverb():
+    adverb = input("Enter an adverb: ")
+    return adverb
+
+
+def print_results(verb, adjective, noun, adverb):
+    print(f"Do you {verb} your {adjective} {noun} {adverb}?\nThat\'s hilarious!")
+
+
+def main():
+    print_results(get_noun(), get_verb(), get_adverb(), get_adjective())
+
+
+if __name__ == "__main__":
+    main()    
