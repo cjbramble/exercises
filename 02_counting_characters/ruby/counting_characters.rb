2080ca85d9bdbdc8ed7f706947c1472bf9e75e19
Chet Bramble <cjbramble@gmail.com>
2020-08-08 15:01:46 -0400

    ruby linting (#12)

--- 02_counting_characters/ruby/counting_characters.rb
+++ 02_counting_characters/ruby/counting_characters.rb
@@ -1,7 +1,7 @@
 begin
-    puts "What is your input string?"
+  puts "What is your input string?"
+
+  input_string = gets.chomp
+end while input_string.empty?
   
-    input_string = gets.chomp
-  end while input_string.empty?
-  
-  puts "#{input_string} has #{input_string.length} characters."
+puts "#{input_string} has #{input_string.length} characters."
--- 08_pizza_party/ruby/pizza_party.rb
+++ 08_pizza_party/ruby/pizza_party.rb
@@ -1,37 +1,37 @@
 class PizzaParty
-    def initialize
-      get_stats
-      print_stats
-    end
-  
-    def get_stats
-      puts "How many people?"
-      @people = gets.chomp.to_i
-  
-      puts "How many pizzas?"
-      @pizzas = gets.chomp.to_i
-  
-      puts "How many slices do the pizzas have?"
-      @slices_per_pizza = gets.chomp.to_i
-    end
-  
-    def total_slices
-      @pizzas * @slices_per_pizza
-    end
-  
-    def slices_per_person
-      total_slices / @people
-    end
-  
-    def remaining_slices
-      total_slices % @people
-    end
-  
-    def print_stats
-      puts "#{@people} people with #{@pizzas} pizzas."
-      puts "Each person gets #{slices_per_person} pieces of pizza."
-      puts "There are #{remaining_slices} leftover."
-    end
+  def initialize
+    get_stats
+    print_stats
   end
+
+  def get_stats
+    puts "How many people?"
+    @people = gets.chomp.to_i
+
+    puts "How many pizzas?"
+    @pizzas = gets.chomp.to_i
+
+    puts "How many slices do the pizzas have?"
+    @slices_per_pizza = gets.chomp.to_i
+  end
+
+  def total_slices
+    @pizzas * @slices_per_pizza
+  end
+
+  def slices_per_person
+    total_slices / @people
+  end
+
+  def remaining_slices
+    total_slices % @people
+  end
+
+  def print_stats
+    puts "#{@people} people with #{@pizzas} pizzas."
+    puts "Each person gets #{slices_per_person} pieces of pizza."
+    puts "There are #{remaining_slices} leftover."
+  end
+end
   
 PizzaParty.new
